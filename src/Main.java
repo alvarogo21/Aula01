d1c0eea6ead4c2263bd2507e648e8334eb0b79a5
alvarogo21 <alvarogo21@gmail.com>
2022-08-17 19:52:40 -0300

    testando

---
+++ src/Main.java
@@ -0,0 +1,110 @@
+import java.util.Scanner;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String nome = "Maria";
+		String nome2 = "Maria";
+	
+		int x = 0, y = 0;
+		
+		
+		System.out.println("digite um numero: ");
+		x = sc.nextInt();
+		
+//		int [] arrays = new int[6];
+//		arrays [0] = 10;
+//		arrays [1] = 20;
+//		arrays [2] = 30;
+//		arrays [3] = 40;
+//		arrays [4] = 50;
+//		arrays [5] = 60;
+//		
+		
+		//ou
+		
+		
+		String [] arrays = {"Maria", "joao"};
+				
+		for(int contador =0; contador < arrays.length; contador ++) {
+		System.out.println(arrays [contador]);
+		}
+		
+		
+		/*
+		 * do { System.out.println("X é igual a: " + x); x = sc.nextInt(); }while (x !=
+		 * 0);
+		 */
+		
+		
+		/*
+		 * for(int contador = 0; contador <10; contador ++) {
+		 * System.out.println(contador); }
+		 */
+		
+		
+		/*
+		 * while(x != 0) { System.out.println("X é igual a: " + x); x = sc.nextInt(); }
+		 */
+		
+	//	System.out.println("saiu do laço");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	//	System.out.println("digite um nome: ");
+		//nome = sc.next();
+		
+		//System.out.println("digite outro nome: ");
+		//nome2 = sc.next();
+		
+		//if(nome.equals(nome2)) {
+			//System.out.println("os nomes são iguais");
+		//}
+		
+	/*	
+		System.out.println("digite um nome: ");
+		x = sc.nextInt();
+		switch(x) {
+		case 1: 
+			System.out.println("O nuemro é 1");
+			break;
+		case 2: 
+			System.out.println("O nuemro é 2");
+			break;
+		case 3: 
+			System.out.println("O nuemro é 3");
+			break;
+		default	:
+			System.out.println("O nuemro nao existe");
+		}	
+		*/
+		
+//		if(x > y) {
+	//		System.out.println("x é maior que y");
+		//} else if(x == 3) {
+			//System.out.println("x é igual a 3");
+//		} else {
+	//		System.out.println("nenhuma delas");
+	//	}
+			
+	}
+}
+			
+			
+		
